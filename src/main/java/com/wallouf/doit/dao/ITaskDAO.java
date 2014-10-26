85c10bc4c1c072164468da8a6d486e062bf38d47
Wallouf <ct13860@gmail.com>
2014-10-26 19:32:37 -0400

    Prepare for Tasks

---
+++ src/main/java/com/wallouf/doit/dao/ITaskDAO.java
@@ -0,0 +1,20 @@
+package com.wallouf.doit.dao;
+
+import java.util.List;
+
+import com.wallouf.doit.entities.Task;
+
+public interface ITaskDAO {
+
+    List<Task> findTasks();
+
+    Task findTask( final String email );
+
+    void createTask( final Task pTask );
+
+    void editTask( final Task pTask );
+
+    void editTaskState( final Task pTask );
+
+    void removeTask( final Task pTask );
+}
---
+++ src/main/java/com/wallouf/doit/dao/TaskDAO.java
@@ -0,0 +1,39 @@
+package com.wallouf.doit.dao;
+
+import java.util.List;
+
+import com.wallouf.doit.entities.Task;
+
+public class TaskDAO implements ITaskDAO {
+
+    public List<Task> findTasks() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Task findTask( String email ) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void createTask( Task pTask ) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void removeTask( Task pTask ) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void editTask( Task pTask ) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void editTaskState( Task pTask ) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
