7a5a20aaf7451a611f486b8f964a385250379bcb
Wallouf <ct13860@gmail.com>
2014-10-26 20:01:56 -0400

    Set Task DAO

---
+++ src/main/java/com/wallouf/doit/dao/TaskDAO.java
@@ -0,0 +1,48 @@
+package com.wallouf.doit.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import com.wallouf.doit.entities.Task;
+
+public class TaskDAO implements ITaskDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Task> findTasks() {
+        final CriteriaBuilder lCriteriaBuilder = entityManager.getCriteriaBuilder();
+
+        final CriteriaQuery<Task> lCriteriaQuery = lCriteriaBuilder.createQuery( Task.class );
+        final Root<Task> lRoot = lCriteriaQuery.from( Task.class );
+        lCriteriaQuery.select( lRoot );
+        final TypedQuery<Task> lTypedQuery = entityManager.createQuery( lCriteriaQuery );
+
+        return lTypedQuery.getResultList();
+    }
+
+    public Task findTask( Integer pTaskId ) {
+        return entityManager.find( Task.class, pTaskId );
+    }
+
+    public void createTask( Task pTask ) {
+        entityManager.persist( pTask );
+    }
+
+    public void removeTask( Task pTask ) {
+        final Task lTask = entityManager.getReference( Task.class, pTask.getId() );
+        entityManager.remove( lTask );
+
+    }
+
+    public void editTask( Task pTask ) {
+        entityManager.persist( pTask );
+    }
+
+}
